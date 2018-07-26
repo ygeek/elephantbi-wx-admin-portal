75da6b28a6ea7ed5f28184446cd24d45c1df05f5
Ethan <940941188@qq.com>
2018-07-26 14:24:35 +0800

    add dockerfile

---
+++ container_start.sh
@@ -0,0 +1,8 @@
+indexFile=/usr/share/nginx/html/index.html
+
+sed -i "s/https:\/\/api.elephantbi.com/${BACKEND_URL}/g" $indexFile
+sed -i "s/FIXED_URL/${SSO_URL}/g" $indexFile
+sed -i "s/WX_CORP_ID/${WX_CORP_ID}/g" $indexFile
+sed -i "s/ENV/${ENV}/g" $indexFile
+
+nginx -g "daemon off;"
