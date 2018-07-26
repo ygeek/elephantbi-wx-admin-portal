1f5d1a86026f3c99bcc6e1dd39be73ebc9e501b2
akamuinsaner <15108387504@163.com>
2018-07-26 20:49:55 +0800

    FIX

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import dva from 'dva';
 import createHistory from 'history/createBrowserHistory';
 import './global.css';
 
+const getInitialState = () => {
+  return localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {};
+};
+
 const app = dva({
   history: createHistory(),
+  initialState: getInitialState(),
 });
 
 app.use({
